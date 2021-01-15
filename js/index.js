4b4cc4e42f45c2a96a91026f506f812e2d6cee34
unknown <ritaparumi@gmail.com>
2021-01-16 00:14:58 +0530

    formatted

--- js/index.js
+++ js/index.js
@@ -1,25 +1,10 @@
-var a_parent =  document.querySelectorAll(".a_parent");
-var dd_menu_a = document.querySelectorAll(".dd_menu_a");
+/*let selection = document.querySelector('select');
+let result = document.querySelector('h2');
 
-a_parent.forEach(function(aitem){
+selection.addEventListener('change', () =>{
+	result.innerText = selection.options[selection.selectedIndex].text;
+});*/
 
-		aitem.addEventListener("click", function(){
-			a_parent.forEach(function(aitem){
-				aitem.classList.remove("active");
-			})
-			dd_menu_a.forEach(function(dd_menu_item){
-				dd_menu_item.classList.remove("active");
-			})
-			aitem.classList.add("active");
-		})
-})
-
-dd_menu_a.forEach(function(dd_menu_item){
-
-		dd_menu_item.addEventListener("click", function(){
-			dd_menu_a.forEach(function(dd_menu_item){
-				dd_menu_item.classList.remove("active");
-			})
-			dd_menu_item.classList.add("active");
-		})
-})
+function selectChanged(newvalue) {
+	alert("you chose: " + newvalue);
+  }
